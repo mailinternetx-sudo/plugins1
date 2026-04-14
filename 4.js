59459db6b01ff8c27e7b05225624c369800381bf
mailinternetx-sudo <mail.internetx@gmail.com>
2026-04-14 14:55:35 +0300

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,86 +1,50 @@
 (function () {
     'use strict';
 
-    const PLUGIN_NAME = 'V10 Ultra';
-    const SOURCE_NAME = 'v10_ultra';
+    const PLUGIN = 'V10 MAX';
+    const SOURCE = 'v10_max';
 
     const GS_URL = 'https://script.google.com/macros/s/AKfycbzGzQVf65Fk9xL9Z9az0dANO7T2BzCIzr-H1xdUnVhWcdLy15NE2yZf_x4ZpgxO3kgT/exec';
 
-    const TMDB_API_KEY = 'f348b4586d1791a40d99edd92164cb86';
-    const TMDB_IMG = 'https://image.tmdb.org/t/p/w500';
-
     const CATEGORIES = {
-        top24: { title: '🔥 Топ 24ч', sheet: 'Топ 24ч' },
+        top: { title: '🔥 Топ 24ч', sheet: 'Топ 24ч' },
         movies: { title: '🎬 Фильмы', sheet: 'Зарубежные фильмы' },
         serials: { title: '📺 Сериалы', sheet: 'Зарубежные сериалы' }
     };
 
-    const cache = {};
-
-    function fetchSheet(sheet) {
-        if (cache[sheet]) return Promise.resolve(cache[sheet]);
-
-        return fetch(GS_URL + '?sheet=' + encodeURIComponent(sheet))
-            .then(r => r.json())
-            .then(j => {
-                cache[sheet] = j.results || [];
-                return cache[sheet];
-            });
-    }
-
-    function fetchTMDB(item) {
-        const key = item.id + '_' + item.type;
-        if (cache[key]) return Promise.resolve(cache[key]);
-
-        const url = `https://api.themoviedb.org/3/${item.type}/${item.id}?api_key=${TMDB_API_KEY}&language=ru-RU`;
-
-        return fetch(url)
-            .then(r => r.json())
-            .then(d => {
-                const result = {
-                    id: d.id,
-                    title: d.title || d.name,
-                    poster_path: d.poster_path ? TMDB_IMG + d.poster_path : '',
-                    backdrop_path: d.backdrop_path ? TMDB_IMG + d.backdrop_path : '',
-                    overview: d.overview,
-                    vote_average: d.vote_average,
-                    release_date: d.release_date || d.first_air_date
-                };
-                cache[key] = result;
-                return result;
-            });
-    }
-
     function Api() {
-        this.list = function (params, onComplete) {
+
+        this.list = function (params, onComplete, onError) {
             const cat = CATEGORIES[params.url];
 
             Lampa.Loader.show();
 
-            fetchSheet(cat.sheet).then(async list => {
+            fetch(GS_URL + '?sheet=' + encodeURIComponent(cat.sheet))
+                .then(r => r.json())
+                .then(json => {
+                    Lampa.Loader.hide();
 
-                // ⚡ ПАРАЛЛЕЛЬНАЯ загрузка (важно!)
-                const promises = list.map(i => fetchTMDB(i));
-                const results = await Promise.all(promises);
-
-                Lampa.Loader.hide();
-
-                onComplete({
-                    results: results,
-                    page: 1,
-                    total_pages: 1
+                    onComplete({
+                        results: json.results,
+                        page: 1,
+                        total_pages: 1
+                    });
+                })
+                .catch(err => {
+                    Lampa.Loader.hide();
+                    onError(err);
                 });
-            });
         };
 
         this.full = function (params, onSuccess, onError) {
+            // 🔥 используем стандарт TMDB full
             Lampa.Api.sources.tmdb.full(params, onSuccess, onError);
         };
     }
 
-    Lampa.Api.sources[SOURCE_NAME] = new Api();
+    Lampa.Api.sources[SOURCE] = new Api();
 
-    Lampa.Component.add('v10_ultra', {
+    Lampa.Component.add('v10_max', {
         template: 'categories',
         data: function () {
             return {
@@ -105,7 +69,7 @@
                 Lampa.Activity.push({
                     title: CATEGORIES[key].title,
                     component: 'category',
-                    source: SOURCE_NAME,
+                    source: SOURCE,
                     url: key
                 });
             });
@@ -114,8 +78,8 @@
 
     function start() {
         Lampa.Activity.push({
-            title: PLUGIN_NAME,
-            component: 'v10_ultra'
+            title: PLUGIN,
+            component: 'v10_max'
         });
     }
 
