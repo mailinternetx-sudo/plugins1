38e569853b4fd40e4d9162e9895dae43bd02f326
mailinternetx-sudo <mail.internetx@gmail.com>
2026-04-27 12:11:40 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -3,7 +3,7 @@
 
     // Конфигурация
     var SOURCE_NAME = 'Rutor Pro';
-    var WORKER_URL = 'https://my-proxy-worker.mail-internetx.workers.dev/'; // ЗАМЕНИТЕ НА ВАШ URL
+    var WORKER_URL = 'https://my-proxy-worker.mail-internetx.workers.dev'; // ЗАМЕНИТЕ НА ВАШ URL
     var ICON = '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 14.5v-9l6 4.5-6 4.5z" fill="currentColor"/></svg>';
 
     // Хранилище настроек
