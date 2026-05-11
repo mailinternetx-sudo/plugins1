0d21f3d034a5120db0fd88a05ea7950c49527300
mailinternetx-sudo <mail.internetx@gmail.com>
2026-05-11 14:52:50 +0300

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,36 +1,78 @@
 (function () {
+
     'use strict';
 
     var SOURCE_NAME = 'V10';
-    var WORKER_URL  = 'https://my-proxy-worker.mail-internetx.workers.dev/';
 
-    var TMDB_IMG = 'https://image.tmdb.org/t/p/w500';
-    var TMDB_BG  = 'https://image.tmdb.org/t/p/original';
+    var WORKER_URL =
+        'https://my-proxy-worker.mail-internetx.workers.dev/';
+
+    var TMDB_IMG =
+        'https://image.tmdb.org/t/p/w500';
+
+    var TMDB_BG =
+        'https://image.tmdb.org/t/p/original';
 
     var CATEGORIES = [
-        { title: 'Топ 24 часа',          url: 'top24'       },
-        { title: 'Зарубежные фильмы',    url: 'movies'      },
-        { title: 'Наши фильмы',          url: 'movies_ru'   },
-        { title: 'Зарубежные сериалы',   url: 'tv_shows'    },
-        { title: 'Русские сериалы',      url: 'tv_shows_ru' },
-        { title: 'Телевизор',            url: 'televizor'   },
-        { title: 'Юмор',                 url: 'humor'       }
+
+        {
+            title: 'Топ 24 часа',
+            url: 'top24'
+        },
+
+        {
+            title: 'Зарубежные фильмы',
+            url: 'movies'
+        },
+
+        {
+            title: 'Наши фильмы',
+            url: 'movies_ru'
+        },
+
+        {
+            title: 'Зарубежные сериалы',
+            url: 'tv_shows'
+        },
+
+        {
+            title: 'Русские сериалы',
+            url: 'tv_shows_ru'
+        },
+
+        {
+            title: 'Телевизор',
+            url: 'televizor'
+        },
+
+        {
+            title: 'Юмор',
+            url: 'humor'
+        }
     ];
 
     function buildImg(item) {
 
-        if (item.img && item.img.startsWith('http')) {
+        if (
+            item.img &&
+            item.img.startsWith('http')
+        ) {
             return item.img;
         }
 
         if (item.poster_path) {
 
-            if (item.poster_path.startsWith('http')) {
+            if (
+                item.poster_path.startsWith('http')
+            ) {
                 return item.poster_path;
             }
 
-            if (item.poster_path.startsWith('/t/p/')) {
-                return 'https://image.tmdb.org' + item.poster_path;
+            if (
+                item.poster_path.startsWith('/t/p/')
+            ) {
+                return 'https://image.tmdb.org' +
+                    item.poster_path;
             }
 
             return TMDB_IMG + item.poster_path;
@@ -50,12 +92,17 @@
 
         if (item.backdrop_path) {
 
-            if (item.backdrop_path.startsWith('http')) {
+            if (
+                item.backdrop_path.startsWith('http')
+            ) {
                 return item.backdrop_path;
             }
 
-            if (item.backdrop_path.startsWith('/t/p/')) {
-                return 'https://image.tmdb.org' + item.backdrop_path;
+            if (
+                item.backdrop_path.startsWith('/t/p/')
+            ) {
+                return 'https://image.tmdb.org' +
+                    item.backdrop_path;
             }
 
             return TMDB_BG + item.backdrop_path;
@@ -66,11 +113,25 @@
 
     function detectMediaMethod(item) {
 
+        if (!item) return 'movie';
+
         if (
+
             item.type === 'tv' ||
-            item.number_of_seasons ||
-            item.seasons ||
-            item.first_air_date
+
+            item.media_type === 'tv' ||
+
+            item.number_of_seasons > 0 ||
+
+            (
+                Array.isArray(item.seasons) &&
+                item.seasons.length
+            ) ||
+
+            (
+                item.first_air_date &&
+                String(item.first_air_date).length > 3
+            )
         ) {
             return 'tv';
         }
@@ -83,74 +144,145 @@
         var img = buildImg(item);
         var bg  = buildBg(item);
 
-        var posterPath = item.poster_path || '';
+        var posterPath =
+            item.poster_path || '';
 
         if (
+
             posterPath &&
+
             !posterPath.startsWith('/t/p/') &&
+
             !posterPath.startsWith('http')
+
         ) {
-            posterPath = '/t/p/w500' + posterPath;
+            posterPath =
+                '/t/p/w500' + posterPath;
         }
 
-        var backdropPath = item.backdrop_path || '';
+        var backdropPath =
+            item.backdrop_path || '';
 
         if (
+
             backdropPath &&
+
             !backdropPath.startsWith('/t/p/') &&
+
             !backdropPath.startsWith('http')
+
         ) {
-            backdropPath = '/t/p/original' + backdropPath;
+            backdropPath =
+                '/t/p/original' + backdropPath;
         }
 
-        var title = item.title || item.name || '';
+        var title =
+            item.title ||
+            item.name ||
+            '';
 
-        var type   = item.type || 'movie';
-        var method = detectMediaMethod(item);
+        var type =
+            item.type || 'movie';
+
+        var method =
+            detectMediaMethod(item);
 
         return {
 
-            id: item.id,
+            id:
+                item.id,
 
-            title: title,
-            name: item.name || title,
-            original_title: item.original_title || title,
+            title:
+                title,
 
-            overview: item.overview || '',
+            name:
+                item.name || title,
 
-            poster_path: posterPath,
-            backdrop_path: backdropPath,
+            original_title:
+                item.original_title || title,
 
-            img: img,
-            background_image: bg,
+            overview:
+                item.overview || '',
 
-            vote_average: item.vote_average || 0,
+            poster_path:
+                posterPath,
 
-            release_date: item.release_date || '',
-            first_air_date: item.first_air_date || '',
+            backdrop_path:
+                backdropPath,
+
+            img:
+                img,
+
+            background_image:
+                bg,
+
+            vote_average:
+
+                isNaN(
+                    parseFloat(item.vote_average)
+                )
+
+                    ? 0
+
+                    : parseFloat(item.vote_average),
+
+            release_date:
+                item.release_date || '',
+
+            first_air_date:
+                item.first_air_date || '',
 
             number_of_seasons:
                 item.number_of_seasons || undefined,
 
             origin_country:
-                item.origin_country || [''],
+
+                item.origin_country &&
+                item.origin_country.length
+
+                    ? item.origin_country
+
+                    : (
+
+                        item.original_language === 'ru'
+
+                            ? ['RU']
+
+                            : ['US']
+                    ),
 
             original_language:
-                item.original_language || '',
 
-            type: type,
-            method: method,
+                item.original_language ||
+
+                (
+                    item.origin_country &&
+                    item.origin_country[0] === 'RU'
+
+                        ? 'ru'
+
+                        : 'en'
+                ),
+
+            type:
+                type,
+
+            method:
+                method,
 
             release_quality:
                 item.release_quality || '',
 
-            source: SOURCE_NAME,
+            source:
+                SOURCE_NAME,
 
             promo_title:
                 item.promo_title || title,
 
             promo:
-                item.promo || item.overview || ''
+                item.promo ||
+                item.overview ||
+                ''
         };
     }
 
@@ -158,22 +290,35 @@
 
         var self = this;
 
-        self.network = new Lampa.Reguest();
+        self.network =
+            new Lampa.Reguest();
 
-        self.fetch = function (url, onComplete, onError) {
+        self.fetch = function (
+            url,
+            onComplete,
+            onError
+        ) {
 
             self.network.silent(
+
                 url,
 
                 function (json) {
 
-                    if (!json || !json.results) {
+                    if (
+                        !json ||
+                        !json.results
+                    ) {
+
                         onComplete([]);
                         return;
                     }
 
                     onComplete(
-                        json.results.map(normalizeCard)
+
+                        json.results.map(
+                            normalizeCard
+                        )
                     );
                 },
 
@@ -185,8 +330,12 @@
                         err
                     );
 
-                    if (onError) onError(err);
-                    else onComplete([]);
+                    if (onError) {
+                        onError(err);
+                    }
+                    else {
+                        onComplete([]);
+                    }
                 }
             );
         };
@@ -209,16 +358,23 @@
             }
 
             var url =
+
                 WORKER_URL +
+
                 'search?query=' +
+
                 encodeURIComponent(query);
 
             self.network.silent(
+
                 url,
 
                 function (json) {
 
-                    if (!json || !json.results) {
+                    if (
+                        !json ||
+                        !json.results
+                    ) {
 
                         onComplete({
                             results: []
@@ -230,7 +386,10 @@
                     onComplete({
 
                         results:
-                            json.results.map(normalizeCard),
+
+                            json.results.map(
+                                normalizeCard
+                            ),
 
                         page:
                             json.page || 1,
@@ -248,266 +407,3 @@
                 }
             );
         };
-
-        self.category = function (
-            params,
-            onSuccess
-        ) {
-
-            var rows  = [];
-            var total = CATEGORIES.length;
-            var done  = 0;
-
-            CATEGORIES.forEach(function (cat) {
-
-                var url =
-                    WORKER_URL +
-                    cat.url +
-                    '?page=1&page_size=25';
-
-                self.fetch(url, function (items) {
-
-                    rows.push({
-
-                        title:
-                            cat.title,
-
-                        results:
-                            items,
-
-                        url:
-                            cat.url,
-
-                        source:
-                            SOURCE_NAME
-                    });
-
-                    done++;
-
-                    if (done === total) {
-
-                        rows.sort(function (a, b) {
-
-                            var ia =
-                                CATEGORIES.findIndex(function (c) {
-                                    return c.url === a.url;
-                                });
-
-                            var ib =
-                                CATEGORIES.findIndex(function (c) {
-                                    return c.url === b.url;
-                                });
-
-                            return ia - ib;
-                        });
-
-                        onSuccess(rows);
-                    }
-                });
-            });
-        };
-
-        self.list = function (
-            params,
-            onComplete
-        ) {
-
-            var page =
-                params.page || 1;
-
-            var pageSize =
-                params.page_size || 25;
-
-            var url =
-                WORKER_URL +
-                params.url +
-                '?page=' + page +
-                '&page_size=' + pageSize;
-
-            self.network.silent(
-                url,
-
-                function (json) {
-
-                    if (!json || !json.results) {
-
-                        onComplete({
-                            results: []
-                        });
-
-                        return;
-                    }
-
-                    onComplete({
-
-                        results:
-                            json.results.map(normalizeCard),
-
-                        page:
-                            json.page || page,
-
-                        total_pages:
-                            json.total_pages || 1,
-
-                        total_results:
-                            json.total_results ||
-                            json.results.length
-                    });
-                },
-
-                function () {
-
-                    onComplete({
-                        results: []
-                    });
-                }
-            );
-        };
-
-        self.full = function (
-            params,
-            onSuccess
-        ) {
-
-            var card =
-                params.card || params;
-
-            var method =
-                detectMediaMethod(card);
-
-            params.method = method;
-
-            if (
-                card &&
-                typeof card === 'object'
-            ) {
-
-                card.method = method;
-                card.type   = method;
-            }
-
-            var savedImg =
-                params.img ||
-                (card && card.img) ||
-                '';
-
-            var savedBg =
-                params.background_image ||
-                (card && card.background_image) ||
-                '';
-
-            var savedQuality =
-                params.release_quality ||
-                (card && card.release_quality) ||
-                '';
-
-            function fallbackFull(data) {
-
-                data = data || {};
-
-                if (!data.title) {
-                    data.title =
-                        card.title ||
-                        card.name ||
-                        '';
-                }
-
-                if (!data.img && savedImg) {
-                    data.img = savedImg;
-                }
-
-                if (
-                    !data.background_image &&
-                    savedBg
-                ) {
-                    data.background_image = savedBg;
-                }
-
-                if (
-                    !data.release_quality &&
-                    savedQuality
-                ) {
-                    data.release_quality = savedQuality;
-                }
-
-                data.type   = method;
-                data.method = method;
-
-                data.origin_country =
-                    data.origin_country || [''];
-
-                data.original_language =
-                    data.original_language || '';
-
-                for (var k in card) {
-
-                    if (
-                        card.hasOwnProperty(k) &&
-                        data[k] === undefined
-                    ) {
-                        data[k] = card[k];
-                    }
-                }
-
-                onSuccess(data);
-            }
-
-            if (
-                !card.id ||
-                card.id <= 0 ||
-                String(card.id).length < 3
-            ) {
-
-                fallbackFull({});
-                return;
-            }
-
-            Lampa.Api.sources.tmdb.full(
-
-                params,
-
-                function (data) {
-
-                    if (!data || !data.title) {
-
-                        fallbackFull(data);
-                    }
-                    else {
-
-                        if (!data.img && savedImg) {
-                            data.img = savedImg;
-                        }
-
-                        if (
-                            !data.background_image &&
-                            savedBg
-                        ) {
-                            data.background_image = savedBg;
-                        }
-
-                        if (
-                            !data.release_quality &&
-                            savedQuality
-                        ) {
-                            data.release_quality = savedQuality;
-                        }
-
-                        data.type   = method;
-                        data.method = method;
-
-                        data.origin_country =
-                            data.origin_country || [''];
-
-                        data.original_language =
-                            data.original_language || '';
-
-                        onSuccess(data);
-                    }
-                },
-
-                function () {
-                    fallbackFull({});
-                }
-            );
-        };
-    }
